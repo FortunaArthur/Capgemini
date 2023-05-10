2b5b5aa1345a0c210ad69c65506c02feaaa60d9b
FortunaArthur <arthur.trabalho2646@gmail.com>
2023-05-10 17:15:31 -0300

    JAVA É MERDA

---
+++ LPA3 - Java/2 - Tipos de Variaveis/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args){
+
+        int idade = 0;
+        float cotacaoDolar = 5.0f;
+        double cotacaoEuro = 6.0f;
+        char sexo = 'M';
+        byte ponto = 0;
+        boolean cadastro = false; 
+
+        String nome = "ODEIO JAVA";
+
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(cotacaoDolar);
+        System.out.println(cotacaoEuro);
+        System.out.println(sexo);
+        System.out.println(ponto);
+        System.out.println(cadastro);
+
+    }
+}
